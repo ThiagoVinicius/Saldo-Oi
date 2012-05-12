24429106876c2d21d2e53b6fe4f78186c819daa2
Thiago Vinícius Freire de Araújo Ribeiro <thiagovfar@gmail.com>
2012-05-12 13:18:06 -0300

    Script para obter dependências mais amigável

--- SaldoOi/lib/get-deps.sh
+++ SaldoOi/lib/get-deps.sh
@@ -1,20 +1,41 @@
 #!/bin/sh
 
-verificaBaixa () 
+verificaBaixa ()
 {
-  echo -n Verificando $1...
+  echo -n $1:' '
+  if [ -f $1 ]; then
+    echo -n 'encontrado, '
+  else
+    echo baixando.
+    wget $2
+    echo -n $1:' '
+    if [ $? -ne 0 ]; then
+      echo Falha ao baixar $1. Desistindo!
+      rm -f $1
+      ERROR=$((ERROR + 1))
+      return
+    else
+      echo -n 'encontrado, '
+    fi
+  fi
+
   sha1sum --check $1.sha1 > /dev/null 2> /dev/null
   if [ $? -ne 0 ]; then
-    echo Falha
-    if [ -f $1 ]; then
-      rm $1
-    fi
-    wget $2
+    echo CORRUPTO!
+    rm -f $1
+    ERROR=$((ERROR + 1))
   else
-    echo OK
-  fi 
+    echo ok.
+  fi
 }
+
+ERROR=0
 
 verificaBaixa slf4j-android-1.6.1-RC1.jar http://www.slf4j.org/android/slf4j-android-1.6.1-RC1.jar
 verificaBaixa ormlite-core-4.30.jar http://ormlite.com/releases/4.30/ormlite-core-4.30.jar
 verificaBaixa ormlite-android-4.30.jar http://ormlite.com/releases/4.30/ormlite-android-4.30.jar
+
+if [ $ERROR -gt 0 ]; then
+  echo '######'
+  echo $ERROR 'erro(s) encontrado(s). Execute-me novamente.'
+fi
